ce0d52d2ce3a1e42367f52f8fbb55bc6fa9c824c
rilieo <rilesdou@gmail.com>
2026-01-02 22:00:26 -0600

    preload images to prevent flickering on mobile

--- components/camera.tsx
+++ components/camera.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { favImages } from "../constants";
 
 const Camera = () => {
-  const [index, setIndex] = useState<number>(0);
+  const [index, setIndex] = useState(0);
 
-  console.log(favImages)
+  useEffect(() => {
+    favImages.forEach(([image]) => {
+      const img = new window.Image();
+      img.src = image.src;
+    });
+  }, []);
 
   const handleIncrement = () => {
     setIndex(index < favImages.length - 1 ? index + 1 : 0);
-  }
+  };
 
   const handleDecrement = () => {
     setIndex(index > 0 ? index - 1 : favImages.length - 1);
-  }
+  };
 
   return (
     <div className="relative flex w-150 md:w-200 p-2">
@@ -23,17 +28,19 @@ const Camera = () => {
         src={favImages[index][0]}
         alt={favImages[index][1]}
         className="object-contain"
+        priority
       />
-      <button 
-        onClick={handleIncrement} 
-        className="absolute bg-transparent w-[2%] h-[14%] right-[29%] top-[55%] rounded-tr-full rounded-br-full" 
+
+      <button
+        onClick={handleIncrement}
+        className="absolute bg-transparent w-[2%] h-[14%] right-[29%] top-[55%]"
       />
-      <button 
+      <button
         onClick={handleDecrement}
-        className="absolute bg-transparent w-[2%] h-[14%] right-[34%] top-[55%] rounded-tl-full rounded-bl-full" 
+        className="absolute bg-transparent w-[2%] h-[14%] right-[34%] top-[55%]"
       />
     </div>
   );
-}
+};
 
 export default Camera;
