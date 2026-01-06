26b14375c0453f684bded4f31a0a8ca2a8b06aaa
rilieo <rilesdou@gmail.com>
2026-01-05 19:50:55 -0600

    fix image flickering on mobile

--- components/camera.tsx
+++ components/camera.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { favImages } from "../constants";
 
 const Camera = () => {
   const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    favImages.forEach(([image]) => {
-      const img = new window.Image();
-      img.src = image.src;
-    });
-  }, []);
 
   const handleIncrement = () => {
     setIndex(index < favImages.length - 1 ? index + 1 : 0);
@@ -24,20 +16,18 @@ const Camera = () => {
 
   return (
     <div className="relative flex w-150 md:w-200 p-2">
-      <Image
-        src={favImages[index][0]}
+      <img
+        src={favImages[index][0].src}
         alt={favImages[index][1]}
-        className="object-contain"
-        priority
       />
 
       <button
         onClick={handleIncrement}
-        className="absolute bg-transparent w-[2%] h-[14%] right-[29%] top-[55%]"
+        className="absolute bg-transparent w-[2%] h-[14%] right-[29%] top-[55%] hover:cursor-pointer"
       />
       <button
         onClick={handleDecrement}
-        className="absolute bg-transparent w-[2%] h-[14%] right-[34%] top-[55%]"
+        className="absolute bg-transparent w-[2%] h-[14%] right-[34%] top-[55%] hover:cursor-pointer"
       />
     </div>
   );
